df009aef8d923eb2363a5216d1c196be1dd8b8c8
siestageek <siestageek@gmail.com>
2024-05-17 09:40:59 +0900

    Update register.js

--- msa-frontend/public/js/register.js
+++ msa-frontend/public/js/register.js
@@ -7,7 +7,7 @@ const regbtn = document.querySelector('#registerbtn');
 
 regbtn.addEventListener('click', async (event) => {
     event.preventDefault(); // 기본 이벤트 방지 추가
-    const res = await fetch('http://43.207.156.131:32324/users', {
+    const res = await fetch('http://${window.env.API_URL}:32324/users', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
